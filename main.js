122d552eaf528f4a0263435296fade4d49641b7a
Hassan Gholami <hassangholami568@gmail.com>
2022-11-12 09:03:19 +0330

    add slider

---
+++ main.js
@@ -0,0 +1,15 @@
+const slides = document.querySelector(".slides");
+const slidesCount = slides.childElementCount;
+const maxleft = (slidesCount - 1) * 100 * -1;
+
+let current = 0;
+
+setInterval(function() {
+    if(current > maxleft) {
+        current -= 100;
+        slides.style.left = current + "%"
+    } else {
+        current = 0;
+        slides.style.left = 0
+    }
+}, 2500)
