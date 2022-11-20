30c2329087a9e96f207f233ad050d918b19c296f
Hassan Gholami <hassangholami568@gmail.com>
2022-11-20 10:38:28 +0330

    change main and article

--- main.js
+++ main.js
@@ -1,19 +1,3 @@
-const slides = document.querySelector(".slides");
-const slidesCount = slides.childElementCount;
-const maxleft = (slidesCount - 1) * 100 * -1;
-
-let current = 0;
-
-setInterval(function() {
-    if(current > maxleft) {
-        current -= 100;
-        slides.style.left = current + "%"
-    } else {
-        current = 0;
-        slides.style.left = 0
-    }
-}, 3500);
-
 const showdepartmanMenu = document.querySelector("#show-departman-menu");
 const hamburgerIcon = document.querySelector(".hamburger-icon");
 const hamburgerMenu = document.querySelector(".hamburger-menu");
